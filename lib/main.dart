48b20ddea05684ebed668ddd1cce6d427e6b5228
GunasekaranRagul <raguloffic@gmail.com>
2024-01-10 18:42:24 +0530

    some Changes

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black12,
       body: bodyUi(),
     );
   }
